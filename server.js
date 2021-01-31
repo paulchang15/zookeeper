559761a7d4a89928cebf836e516947dd6885912f
Paul Chang <paulchang959@gmail.com>
2021-01-30 16:15:08 -0800

    added front end searchs features

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
 const { animals } = require("./data/animals");
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/apiRoutes/htmlRoutes");
 const fs = require("fs");
 const path = require("path");
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 app.use(express.static("public"));
 
 function filterByQuery(query, animalsArray) {
